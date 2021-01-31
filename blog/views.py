e1a80d55e6b5229311983769cb6f79e614f61718
YellowBallPython <yellowballpython@gmail.com>
2021-01-31 03:18:48 -0600

    Added Pagination
    Se agrega pagination para el view que despliega todos los posts con/sin tags.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from taggit.models import Tag
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 
@@ -14,9 +16,20 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts = posts.filter(tags__in=[tag])
 
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
+        'page': page,
         'posts': posts,
         'tag': tag,
+
     }
 
     return render(request, 'blog/post/index.html', context)
