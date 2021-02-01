9897808e562931641c0cc785987b4476476e996b
YellowBallPython <yellowballpython@gmail.com>
2021-01-31 23:32:00 -0600

    Blog styled

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='index'),
-    path('<int:year>/<int:month>/<int:day>/<str:slug>/',
-         views.PostDetail.as_view(), name='post_detail'),
     # path('<int:year>/<int:month>/<int:day>/<str:slug>/',
-    #      views.post_detail, name='post_detail'),
+    #      views.PostDetail.as_view(), name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<str:slug>/',
+         views.post_detail, name='post_detail'),
     path('tag/<slug:tag_slug>/', views.post_list, name='posts_by_tag'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+
 from .models import Post
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from taggit.models import Tag
+
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -11,6 +14,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def post_list(request, tag_slug=None):
     posts = Post.published.all()
     tag = None
+    most_visited = Post.objects.all().annotate(most_visited=Count("visited")
+                                               ).order_by("most_visited", "-publish")[:3]
+    most_tags = Post.tags.most_common()[:3]
 
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
@@ -29,23 +35,43 @@ def post_list(request, tag_slug=None):
         'page': page,
         'posts': posts,
         'tag': tag,
+        'most_visited': most_visited,
+        'most_tags': most_tags,
 
     }
 
     return render(request, 'blog/post/index.html', context)
 
 
-class PostDetail(DetailView):
-    model = Post
-    template_name = 'blog/post/detail.html'
-    context_object_name = 'post'
+# class PostDetail(DetailView):
+#     model = Post
+#     template_name = 'blog/post/detail.html'
+#     context_object_name = 'post'
 
 
-# def post_detail(request, year, month, day, slug):
-#     post = get_object_or_404(Post, status='published',
-#                              publish__year=year,
-#                              publish__month=month,
-#                              publish__day=day,
-#                              slug=slug,
-#                              )
-#     return render(request, 'blog/post/detail.html', {'post': post, })
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             slug=slug,
+                             )
+    post.visited += 1
+    post.save()
+    most_visited = Post.objects.all().annotate(most_visited=Count("visited")
+                                               ).order_by("most_visited", "-publish")[:3]
+    most_tags = Post.tags.most_common()[:3]
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:3]
+
+    context = {
+        'post': post,
+        'most_visited': most_visited,
+        'similar_posts': similar_posts,
+        'most_tags': most_tags,
+    }
+    return render(request, 'blog/post/detail.html', context)
