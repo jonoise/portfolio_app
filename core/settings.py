6ec14992e2e362829ae52a7512e96e578e9b1368
YellowBallPython <yellowballpython@gmail.com>
2021-02-02 16:48:21 -0600

    STATIC ROOT CHANGED

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Para que Django busque en cada aplicacion por su static respectivo.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blog/static"),
