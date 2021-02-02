5e94f2bac43a0b5e3b7dd653d1accbf2430b2fb0
YellowBallPython <yellowballpython@gmail.com>
2021-02-02 17:25:32 -0600

    Dirs Templates

--- core/settings.py
+++ core/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'blog/templates', BASE_DIR / 'frontend/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
