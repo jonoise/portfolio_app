2490874a9e1b7aaa5a9719d07058b231bc59375c
YellowBallPython <yellowballpython@gmail.com>
2021-02-03 20:22:44 -0600

    Email settings
    En el view del frontend: se modifica el from= para designar el user de MAILGUN

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
 # Create your views here.
 
@@ -16,7 +17,7 @@ def index(request):
                       message=render_to_string('frontend/contact_mail.html', {'name': form.cleaned_data['name'],
                                                                               'email': form.cleaned_data['email'],
                                                                               'message': form.cleaned_data['message']}),
-                      from_email='yellowballpython@gmail.com',
+                      from_email=settings.EMAIL_HOST_USER,
                       recipient_list=['amilkarms@outlook.com'],
                       fail_silently=False)
             return redirect('frontend:thankyou')
