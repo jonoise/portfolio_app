510d3a86e560ee558b6633be32421df9bbd887c5
YellowBallPython <yellowballpython@gmail.com>
2021-02-03 21:40:59 -0600

    blog.urls
    se cambia el URL para el detail del post. TEST 1

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.post_list, name='index'),
     # path('<int:year>/<int:month>/<int:day>/<str:slug>/',
     #      views.PostDetail.as_view(), name='post_detail'),
-    path('<int:year>/<int:month>/<int:day>/<str:slug>/',
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.post_detail, name='post_detail'),
     path('tag/<slug:tag_slug>/', views.post_list, name='posts_by_tag'),
     path('new/', views.post_new, name='post_new'),
