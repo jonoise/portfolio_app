5f303fac28a2b7ead05f4727ae514b7ea7817b4a
YellowBallPython <yellowballpython@gmail.com>
2021-02-07 23:48:30 -0600

    CodeSnippet Theme
    Testing...

--- staticfiles/ckeditor/ckeditor/config.js
+++ staticfiles/ckeditor/ckeditor/config.js
@@ -3,8 +3,9 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#AADC6E';
+  config.codeSnippet_theme = "Monokai Sublime";
 };
