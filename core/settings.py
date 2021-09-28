6e36d5f960856117b988e75634d37133b59b5e89
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-28 16:03:16 -0600

    add drf and cors

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import django_heroku
+import django_on_heroku
 import dj_database_url
 
 load_dotenv()
